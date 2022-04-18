183eccc34246c7e15789c50af1866836d8bff5a1
hideonspotify <85899602+hideonspotify@users.noreply.github.com>
2022-04-18 11:47:46 -0500

    Add files via upload

---
+++ datos.php
@@ -0,0 +1,22 @@
+<?php
+$nom = $_POST['nombres'];
+$apel = $_POST['apellidos'];
+$telef = $_POST['telefono'];
+$em = $_POST['correo'];
+
+ ?>
+ <!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="esti.css">
+	<title>Datos</title>
+</head>
+<body>
+	<h1>I.E Nuevo Mundo</h1>
+	<h2>Felicitaciones! Ya estas registrado, en breves te enviaremos la informacion que necesitas.</h2>
+
+</body>
+</html>
+
